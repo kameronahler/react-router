4f46b3c13cf36dc9d0419b437b05a336fe1485ec
Kameron Ahler <kamahler@gmail.com>
2020-08-30 13:40:41 -0600

    use match to fetch api on detail page

--- src/pages/PageShopDetail/PageShopDetail.js
+++ src/pages/PageShopDetail/PageShopDetail.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageHeader from '../../components/PageHeader/PageHeader'
 import '../Page.scss'
+export default function PageShopDetail({ match }) {
+  useEffect(() => {
+    getData()
+  }, [])
 
-export default function PageShopDetail() {
+  const [item, setItem] = useState([])
+
+  const getData = async () => {
+    const data = await fetch(`https://reqres.in/api/users/${match.params.id}`)
+    const parsedData = await data.json()
+    setItem(parsedData.data)
+  }
+  console.log(item)
   return (
     <div className='page'>
-      <PageHeader heading={'Shop'} />
+      <PageHeader heading={`${item.first_name} ${item.last_name}`} />
+      <img alt='' width='50' height='50' src={item.avatar}></img>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iste
+        optio ex sequi quia dignissimos beatae adipisci ut illo dolorum libero
+        quam aspernatur laudantium, eius, architecto repellat deserunt? Quam,
+        quis.
+      </p>
     </div>
   )
 }
