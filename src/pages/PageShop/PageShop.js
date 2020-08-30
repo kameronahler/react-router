d34a8d81d6f0e9cbed2bcce8d89056b46ef1c4b9
Kameron Ahler <kamahler@gmail.com>
2020-08-30 15:38:22 -0600

    useRouteMatch function

--- src/pages/PageShop/PageShop.js
+++ src/pages/PageShop/PageShop.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch, Link, useRouteMatch } from 'react-router-dom'
 import PageHeader from '../../components/PageHeader/PageHeader'
 import PageShopDetail from '../PageShopDetail/PageShopDetail'
 import '../Page.scss'
 
-export default function PageShop({ match }) {
+export default function PageShop() {
+  const { path, url } = useRouteMatch()
+
   useEffect(() => {
     getData()
   }, [])
@@ -22,19 +24,19 @@ export default function PageShop({ match }) {
       <PageHeader heading={'Shop'} />
       <Switch>
         {/* We could either break this into another component, or just render it here. App.js is telling a link in the header to head to this component, when this component is called, it asks the router, what url is it? in another switch function, and makes another delineation. This could all be handled from App.js, but this lets us compartementalize where it goes */}
-        <Route path={`${match.path}`} exact>
+        <Route path={`${path}`} exact>
           {currentItems.map((el) => (
             <li key={el.id}>
               <img alt='' width='50' height='50' src={el.avatar}></img>
               <Link
-                to={`${match.url}/${el.id}`}
+                to={`${url}/${el.id}`}
               >{`${el.first_name} ${el.last_name}`}</Link>
             </li>
           ))}
         </Route>
 
         {/* other routes */}
-        <Route path={`${match.path}/:id`} exact component={PageShopDetail} />
+        <Route path={`${path}/:id`} exact component={PageShopDetail} />
       </Switch>
     </div>
   )
